797c34707cf58093beac73bc3da1f84f928bc4f3
bmorawska <37190313+bmorawska@users.noreply.github.com>
2018-05-25 00:56:26 +0200

    Utworzenie i wypełnienie bazy danych.

---
+++ dane.sql
@@ -0,0 +1,213 @@
+
+INSERT INTO poszukiwacz
+VALUES('Vasor','jsportism@guvewfmn7j1dmp.cf','NOWICJUSZ');
+INSERT INTO poszukiwacz
+VALUES('Zanthid','jmetal-licam@i4racpzge8.gq','UCZEN');
+INSERT INTO poszukiwacz
+VALUES('Baldus','wpedro.pedro.9@k3zaraxg9t7e1f.cf','CZELADNIK');
+INSERT INTO poszukiwacz
+VALUES('Eridal','nourdine_me873@vprice.co','UCZEN');
+INSERT INTO poszukiwacz
+VALUES('Parmir','ybigd@akamaized.ga','UCZEN');
+INSERT INTO poszukiwacz
+VALUES('Marenmir','bmahmoud.edlby.79@mg-rover.gq','NOWICJUSZ');
+INSERT INTO poszukiwacz
+VALUES('Ragnor','8amir.guidoum2b@8t0sznngp6aowxsrj.cf','NOWICJUSZ');
+GO
+
+INSERT INTO skrzynka 
+VALUES ('Pomnik Wilków', 
+		'W Parku Wolnoœci znajduje sie pomnik postawiony 
+		 15 Pu³kowi Piechoty "Wilków" w 35 rocznice walk o Pabianice.',
+		32,
+		1,
+		'23/07/2014',
+		'51.663939', 
+		'19.347119',
+		'Baldus',
+		'MIKRO',
+		'TRADYCYJNA');
+INSERT INTO skrzynka 
+VALUES ('Lasek Karolewski',
+		'Jest to mój pierwszy kesz, dlatego gor¹co zapraszam do szukania!',
+		19,
+		1,
+		'31/08/2012',
+		'51.661297', 
+		'19.349093',
+		'Baldus',
+		'MALY',
+		'ZAGADKOWA');
+INSERT INTO skrzynka 
+VALUES ('most drzewo', 
+	    'Zostaw w takim stanie, w jakim zasta³eœ.',
+		11,
+		1,
+		'12/05/2016',
+		'51.661696', 
+		'19.359082',
+		'Baldus',
+		'NORMALNY',
+		'WYDARZENIE');
+INSERT INTO skrzynka 
+VALUES ('Pa³ac Oskara Kindlera w Ksawerowie', 
+	    'Pa³ac zosta³ zbudowany w 1898 roku przez Oskara Kindlera, 
+		 znanego pabianickiego fabrykanta.',
+		 21,
+		 0,
+		 '11/09/2018',
+		 '51.659766', 
+		 '19.360756',
+		 'Baldus',
+		 'DUZY',
+		 'ZDJECIE');
+INSERT INTO skrzynka 
+VALUES ('Willa Hoffmana w Ksawerowie',
+		'Rudolf Hoffman w³aœciciel willi od 1910 r. by³ dyrektorem w zak³adach 
+		 bawe³nianych Izraela Poznañskiego',
+		 11,
+		 1,
+		 '01/07/2005',
+		 '51.663352', 
+		 '19.353143',
+		 'Baldus',
+		 'MIKRO',
+		 'EARTHCACHE');
+INSERT INTO skrzynka 
+VALUES ('£OTR #130', 
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 123,
+		 0,
+		 '01/02/2013',
+		 '51.668665', 
+		 '19.353866',
+		 'Zanthid',
+		 'MALY',
+		 'WYDARZENIE');
+INSERT INTO skrzynka 
+VALUES ('Du¿a Woda',
+		'Skrzynka znajduje siê w pobli¿u pomnika przyrody Du¿a Woda w okolicach Pabianic.',
+		 33,
+		 0,
+		 '12/25/2015',
+		 '51.655097', 
+		 '19.317106',
+		 'Eridal',
+		 'NORMALNY',
+		 'TRADYCYJNA');
+INSERT INTO skrzynka 
+VALUES ('£OTR #124',
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 87,
+		 1,
+		 '12/08/2017',
+		 '51.661529', 
+		 '19.341466',
+		 'Parmir',
+		 'DUZY',
+		 'ZAGADKOWA');
+INSERT INTO skrzynka 
+VALUES ('£OTR #117 - Obwodowa',
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 90,
+		 1,
+		 '05/24/2018',
+		 '51.659495', 
+		 '19.372906',
+		 'Baldus',
+		 'MIKRO',
+		 'EARTHCACHE');
+INSERT INTO skrzynka 
+VALUES ('£OTR #118 - Filemon i Bonifacy',
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 14,
+		 1,
+		 '12/06/2013',
+		 '51.647917', 
+		 '19.371702',
+		 'Baldus',
+		 'MALY',
+		 'ZDJECIE');
+INSERT INTO skrzynka 
+VALUES ('£OTR #116 - Przygód kilka wróbla Cwirka',
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 65,
+		 1,
+		 '11/11/2015',
+		 '51.660353', 
+		 '19.357069',
+		 'Baldus',
+		 'NORMALNY',
+		 'WYDARZENIE');
+INSERT INTO skrzynka 
+VALUES ('£OTR #119 - Plastusiowy pamiêtnik', 
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 231,
+		 0,
+		 '12/24/2015',
+		 '51.664274', 
+		 '19.338878',
+		 'Parmir',
+		 'DUZY',
+		 'WYDARZENIE');
+INSERT INTO skrzynka 
+VALUES ('S8 MOP Guzew - Sorth',
+		'Droga ekspresowa S8 – budowana polska droga ekspresowa 
+		 przebiegaj¹ca od Wroc³awia do Bia³egostoku.',
+		 12,
+		 1,
+		 '03/03/2007',
+		 '51.662198', 
+		 '19.335370',
+		  'Parmir',
+		 'MIKRO',
+		 'ZDJECIE');
+INSERT INTO skrzynka 
+VALUES ('£OTR #115 - Zastawna V',
+		'£ódzka Obwodnica Terenowo Rowerowa to seria skrzynek posadowiona 
+		 wzd³u¿ granic administracyjnych £odzi.',
+		 40,
+		 1,
+		 '12/05/2016',
+		 '51.654028', 
+		 '19.343814',
+		 'Zanthid',
+		 'MALY',
+		 'TRADYCYJNA');
+INSERT INTO skrzynka 
+VALUES ('Cmentarz ewangelicki w Czy¿eminku',
+		'Cmentarz ewangelicki w Czy¿eminku rzekomo powsta³ w XIX wieku.',
+		0,
+		0,
+		'09/09/2016',
+		'51.655633', 
+		'19.353716',
+		'Zanthid',
+		'NORMALNY',
+		'ZAGADKOWA');
+
+
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Baldus','£OTR #130', '05/12/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Baldus','Du¿a Woda', '05/16/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Baldus','£OTR #115 - Zastawna V', '05/18/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Baldus','£OTR #119 - Plastusiowy pamiêtnik', '05/20/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Baldus','S8 MOP Guzew - Sorth', '05/22/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Zanthid','Lasek Karolewski', '05/24/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Zanthid','Pomnik Wilków', '05/26/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Zanthid','most drzewo', '05/28/2018');
+INSERT INTO odkrycie (poszukiwacz, skrzynka, dataZnalezienia)
+VALUES('Zanthid','S8 MOP Guzew - Sorth', '05/30/2018');
