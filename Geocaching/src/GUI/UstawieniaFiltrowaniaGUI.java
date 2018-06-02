e1d3799e53660f00273057aa059d1a226a7faed0
Basia <Basia@KOMP-BASI.home>
2018-06-02 02:03:46 +0200

    Cos nie pykło

--- Geocaching/src/GUI/UstawieniaFiltrowaniaGUI.java
+++ Geocaching/src/GUI/UstawieniaFiltrowaniaGUI.java
@@ -33,7 +33,7 @@ public class UstawieniaFiltrowaniaGUI extends JFrame {
 	public UstawieniaFiltrowaniaGUI(RootGUI rootGUI) {
 		this.rootGUI = rootGUI;
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 600, 600);
+		setBounds(100, 100, 600, 600); 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
